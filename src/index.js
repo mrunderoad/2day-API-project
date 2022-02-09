788dbd2996641ccfe99f45a0b9bc4dbd70b7d3c0
Isaac Overstreet <isaacoverstreet3@gmail.com>
2022-02-09 10:31:09 -0800

    simple API call working
    Co-authored-by: Matt Eilar <meilar@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,4 +1,32 @@
-// import $ from 'jquery';
+import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import CryptoService from './js/api.js';
+
+function clearFields() {
+  $("input:radio[name=coin]:checked").val("");
+}
+
+function getElements(response) {
+  if (response) {
+    $('.showCoin').text(`The price of ${response[0].name} is ${response[0].price}.`);
+  } else {
+    $('.showErrors').text(`There was an error: ${response.message}`);
+  }
+}
+
+async function makeApiCall(coin) {
+  const response = await CryptoService.getCrypto(coin);
+  console.log(response[0].price);
+  console.log(response[0].name);
+  getElements(response);
+}
+
+$(document).ready(function() {
+  $('#checkCrypto').click(function() {
+    let coin = $("input:radio[name=coin]:checked").val();
+    clearFields();
+    makeApiCall(coin);
+  });
+});
---
+++ src/js/api.js
@@ -0,0 +1,13 @@
+export default class CryptoService {
+  static async getCrypto(coin) {
+    try {
+      const response = await fetch(`https://api.nomics.com/v1/currencies/ticker?key=${process.env.API_KEY}&ids=${coin}`);
+      if (!response.ok) {
+        throw Error(response.statusText);
+      }
+      return response.json();
+    } catch(error) {
+      return error.message
+    }
+  }
+}
